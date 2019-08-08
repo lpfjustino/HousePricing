435147cad2a6d382410b2e3298c03284f87e9dbe
lpfjustino <lpfjustino@gmail.com>
2019-08-08 20:41:48 -0300

    Refactored preprocessing file

--- ml/preprocessing.py
+++ ml/preprocessing.py
@@ -2,22 +2,37 @@ from sklearn.impute import SimpleImputer
 from eda.stats import features_missing_too_many
 from resources import *
 
-# Dropping features with too many missing values
-data.drop(features_missing_too_many, axis=1, inplace=True)
+def preprocess():
+    drop_missing()
+    fill_missing_numeric()
+    fill_missing_cathegoric()
+    fill_remaining()
 
-# Imputing missing numerical features
-mean_imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputed_features = ['LotFrontage', 'MasVnrArea', 'GarageArea']
-mean_imputer.fit(data[imputed_features])
-data[imputed_features] = mean_imputer.transform(data[imputed_features])
 
-# Filling missing categoric features
-features = ['Fence', 'FireplaceQu']
-for feature in features:
-    data[feature].fillna('Missing', inplace=True)
+def fill_remaining():
+    # Filling the rest of the features with the mode
+    data.fillna(data.mode().iloc[0, :], inplace=True)
 
-# Filling the rest of the features with the mode
-data.fillna(data.mode().iloc[0, :], inplace=True)
+
+def fill_missing_cathegoric():
+    # Filling missing categoric features
+    features = ['Fence', 'FireplaceQu']
+    for feature in features:
+        data[feature].fillna('Missing', inplace=True)
+
+
+def fill_missing_numeric():
+    # Imputing missing numerical features
+    mean_imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    imputed_features = ['LotFrontage', 'MasVnrArea', 'GarageArea']
+    mean_imputer.fit(data[imputed_features])
+    data[imputed_features] = mean_imputer.transform(data[imputed_features])
+
+
+def drop_missing():
+    # Dropping features with too many missing values
+    data.drop(features_missing_too_many, axis=1, inplace=True)
+
 
 
 
