9c7ea8c9ed85709ed0ff994053f092779124b6e7
lpfjustino <lpfjustino@gmail.com>
2019-08-08 20:30:28 -0300

    Restructuring improvements

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
+from ml.model import *
 
 train = pd.read_csv('../input/train.csv')
 test = pd.read_csv('../input/test.csv')
 data = pd.concat([train.iloc[:, :-1], test], axis=0)
+
+v1_prediction = model_v1()
+output = pd.concat([test['Id'], v1_prediction], axis=1)
+output.to_csv(r'C:\Users\lpfjustino\Desktop\output_v1.csv', index=None, header=True)
--- ml/model.py
+++ ml/model.py
@@ -1,17 +1,13 @@
 from sklearn.svm import SVR
 from index import *
 
-train = pd.read_csv('../input/train.csv')
-test = pd.read_csv('../input/test.csv')
+def model_v1():
+    features = ['OverallQual']
+    X = train[features]
+    y = train['SalePrice']
+    clf = SVR(C=1000)
+    clf.fit(X, y)
+    Xt = test[features]
+    predicted = pd.DataFrame(clf.predict(Xt), columns=['SalePrice'])
 
-features = ['OverallQual']
-X = train[features]
-y = train['SalePrice']
-clf = SVR(C=1000)
-clf.fit(X, y)
-
-Xt = test[features]
-predicted = pd.DataFrame(clf.predict(Xt), columns=['SalePrice'])
-
-output = pd.concat([test['Id'], predicted], axis=1)
-output.to_csv(r'C:\Users\lpfjustino\Desktop\output.csv', index=None, header=True)
+    return predicted
