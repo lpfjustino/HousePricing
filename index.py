dcbed131c3848f566d397c3be238338ad2968105
lpfjustino <lpfjustino@gmail.com>
2019-08-11 20:17:45 -0300

    Tuning around SVM's polynomial kernel

--- index.py
+++ index.py
@@ -1,10 +1,18 @@
-from ml.model import model_v1, model_v2
+from ml.model import model_v1, model_v2, model_v2_1, model_v2_2
 from resources import *
 
 # v1_prediction = model_v1()
 # output = pd.concat([test['Id'], v1_prediction], axis=1)
 # output.to_csv(r'C:\Users\lpfjustino\Desktop\output_v1.csv', index=None, header=True)
 
-v2_prediction = model_v2()
-output = pd.concat([test['Id'], v2_prediction], axis=1)
-output.to_csv(r'C:\Users\lpfjustino\Desktop\output_v2.csv', index=None, header=True)
+# v2_prediction = model_v2()
+# output = pd.concat([test['Id'], v2_prediction], axis=1)
+# output.to_csv(r'C:\Users\lpfjustino\Desktop\output_v2.csv', index=None, header=True)
+
+# v2_1_prediction = model_v2_1()
+# output = pd.concat([test['Id'], v2_1_prediction], axis=1)
+# output.to_csv(r'C:\Users\lpfjustino\Desktop\output_v2_1.csv', index=None, header=True)
+
+v2_2_prediction = model_v2_2()
+output = pd.concat([test['Id'], v2_2_prediction], axis=1)
+output.to_csv(r'C:\Users\lpfjustino\Desktop\output_v2_2.csv', index=None, header=True)
--- ml/model.py
+++ ml/model.py
@@ -2,9 +2,7 @@ from sklearn.svm import SVR
 from resources import *
 from ml.preprocessing import preprocess
 
-from sklearn.model_selection import cross_val_score, GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import GridSearchCV
 
 def model_v1():
     features = ['OverallQual']
@@ -35,5 +33,43 @@ def model_v2():
     reg.fit(X, y)
     predicted = pd.DataFrame(reg.predict(test_data), columns=['SalePrice'])
     print(reg.best_estimator_)
+
+    return predicted
+
+def model_v2_1():
+    data = preprocess()
+    train_n_rows = train.shape[0]
+    train_df = data.iloc[:train_n_rows, 1:]
+    test_data = data.iloc[train_n_rows:, 1:]
+
+    # Reattaching the target variable
+    train_data = pd.concat([train_df, train.iloc[:, -1]], axis=1)
+
+    X = train_data.iloc[:, :-1]
+    y = train_data.iloc[:, -1]
+
+    parameters = {'kernel': ['poly'], 'C': [0.0125, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175], 'degree': [2, 3]}
+    svr = SVR(gamma='auto', max_iter=100000)
+    reg = GridSearchCV(svr, parameters, cv=5, verbose=True)
+    reg.fit(X, y)
+    predicted = pd.DataFrame(reg.predict(test_data), columns=['SalePrice'])
+    print(reg.best_estimator_)
+
+def model_v2_2():
+    data = preprocess()
+    train_n_rows = train.shape[0]
+    train_df = data.iloc[:train_n_rows, 1:]
+    test_data = data.iloc[train_n_rows:, 1:]
+
+    # Reattaching the target variable
+    train_data = pd.concat([train_df, train.iloc[:, -1]], axis=1)
+
+    X = train_data.iloc[:, :-1]
+    y = train_data.iloc[:, -1]
+
+    reg = SVR(C=0.175, cache_size=200, coef0=0.0, degree=2, epsilon=0.1, gamma='auto', kernel='poly', max_iter=-1,
+              shrinking=True, tol=0.001, verbose=False)
+    reg.fit(X, y)
+    predicted = pd.DataFrame(reg.predict(test_data), columns=['SalePrice'])
 
     return predicted
